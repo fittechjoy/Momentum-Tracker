94f59083d7be439fc529e520149c1e0ec21de0cf
fittechjoy <kiama01joy@gmail.com>
2025-10-18 20:41:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       <Navbar />
       <div className="p-6">
+       <ErrorBoundary>
         <Dashboard />
+        </ErrorBoundary> 
       </div>
     </div>
   );
