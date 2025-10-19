11b9469cf4db913bf184a966c49016acac4461a6
fittechjoy <kiama01joy@gmail.com>
2025-10-19 04:28:33 +0300

    Update new clean codes for the app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"  // <-- Make sure this matches your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
